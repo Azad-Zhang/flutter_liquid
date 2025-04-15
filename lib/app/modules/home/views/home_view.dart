b3c3a486f48acdcac2e47d3e11cf3592f7399010
张仕鹏 <1120148291@qq.com>
2025-04-15 16:27:44 +0800

    add gif

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,7 +2,7 @@
  * @Author: 张仕鹏 1120148291@qq.com
  * @Date: 2025-03-19 10:27:01
  * @LastEditors: 张仕鹏 1120148291@qq.com
- * @LastEditTime: 2025-04-15 15:38:37
+ * @LastEditTime: 2025-04-15 15:55:52
  * @FilePath: /my_flutter_template_getx/lib/app/modules/home/views/home_view.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -60,9 +60,9 @@ class HomeView extends BaseView<HomeController> {
           end: Alignment.bottomRight,
         ),
         containerPadding: 10,
-        containerBorder: Border.all(color: Colors.green),
+        containerBorder: Border.all(color: Color(0xFFFF8181)),
         containerSize: 200, 
-        percentage: 1,
+        percentage: 0.5,
         // shadowColor: Colors.red,
       ),
     );
